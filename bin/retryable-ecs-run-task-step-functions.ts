96632a462106d57963144e9b148679223c7528e2
Osaka <gebe1993shuukatsu@gmail.com>
2024-08-04 15:03:27 +0900

    update packages to cdk version 2.151.0

--- bin/retryable-ecs-run-task-step-functions.ts
+++ bin/retryable-ecs-run-task-step-functions.ts
@@ -4,4 +4,8 @@ import * as cdk from "aws-cdk-lib";
 import { RetryableEcsRunTaskStepFunctionsStack } from "../lib/retryable-ecs-run-task-step-functions-stack";
 
 const app = new cdk.App();
-new RetryableEcsRunTaskStepFunctionsStack(app, "RetryableEcsRunTaskStepFunctionsStack");
+new RetryableEcsRunTaskStepFunctionsStack(app, "RetryableEcsRunTaskStepFunctionsStack", {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION
+  }});
